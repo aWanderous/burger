7cbd028b27dd9b0bcd170646f7ea950df0a4fd76
Alex Wan <a_wanderous@hotmail.com>
2019-11-02 23:12:54 +1100

    fix heroku link

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,15 @@ var mysql = require("mysql");
 require("dotenv").config();
 
 if (process.env.JAWSDB_URL) {
-    //Heroku deployment
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-var connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: 3306,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: "burgers_db"
-});
+    var connection = mysql.createConnection({
+        host: process.env.HOST,
+        port: 3306,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: "burgers_db"
+    });
 };
 
 connection.connect(function (err) {
